71a17daccf150dc42f8000e1ef33cb890362fb6d
kaya1kausa <kaya1kausa.0spt@gmail.com>
2025-05-22 10:00:53 -0300

    Initial commit: Added hello.c program

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello universe, Kaya1Kausa it's coming!\n");
+    printf("The kausa game is here and now at 0spt speed!\n");
+    return 0;
+}
